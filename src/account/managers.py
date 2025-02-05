97c679638a1b62266367a8707d5835f980c043e6
isabella-muniz <contato.isabellaramos@hotmail.com>
2025-02-05 17:23:12 +0000

    src

--- src/account/managers.py
+++ src/account/managers.py
@@ -4,11 +4,28 @@ from django.utils.translation import gettext_lazy as _t
 # from django.utils.translation import gettext_lazy as _t 
 
 class CustemUserManager(BaseUserManager):
-    def create_user():
-        pass 
+    def create_user(self, email: str, password: str, **other_fields):
+        if not email.strip():
+            raise ValueError(_t("Empty email. The email must be set.")) 
+        user = self.model(
+            email = self.normalize_email(email), #passa o email de maiuscula para minuscula
+            **other_fields,
+        )
+        user.set_password(password)
+        user.save()
+        return user
     #:
 
-    def create_superuser(self):
-        pass
+    def create_superuser(self, email: str, password: str, **other_fields):
+        
+        must_be_true_fields = ('is_staff', 'is_superuser', 'is_active')
+            for field in must_be_true_fields:
+                if field in other_fields and not other_fields[field]:
+                    raise ValueError(_t(f"Field{field} must be True or left alone"))
+                other_fields[field] = True
+                user = self.create_superuser(email, password, **other_fields)
+                user.is_admin = True
+                user.save()
+                return user
     #:
 #:
