63a57389b91bd65cbe8a2c5a2465ae34d5e4c1ab
isabella-muniz <contato.isabellaramos@hotmail.com>
2025-02-05 17:11:20 +0000

    src

---
+++ src/account/managers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.base_ser import BaseUserManager
+from django.utils.translation import gettext_lazy as _t
+
+# from django.utils.translation import gettext_lazy as _t 
+
+class CustemUserManager(BaseUserManager):
+    def create_user():
+        pass 
+    #:
+
+    def create_superuser(self):
+        pass
+    #:
+#:
