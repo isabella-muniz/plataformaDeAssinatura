df9baad24250e367945650ec6deecf8c8e8aa642
isabella-muniz <contato.isabellaramos@hotmail.com>
2025-02-18 17:27:20 +0000

    Segunda Aula Parte C

--- src/account/views.py
+++ src/account/views.py
@@ -5,16 +5,21 @@ from django.http import HttpRequest, HttpResponse
 
 async def home(request: HttpRequest) -> HttpResponse:
     """..."""
-    return HttpResponse("Estamos na home da app de account")
+    #return HttpResponse("Estamos na home da app de account")
+    return render(request, 'account/home.html', {'x': 33})
 #:
 
 async def register(request: HttpRequest) -> HttpResponse:
     """..."""
-    return HttpResponse("Estamos no register da app de account")
+    #return HttpResponse("Estamos no register da app de account")
+    return render(request, 'account/register.html')
+
 #:
 
 async def login(request: HttpRequest) -> HttpResponse:
     """..."""
-    return HttpResponse("Estamos no login da app de account")
+    #return HttpResponse("Estamos no login da app de account")
+    return render(request, 'account/login.html')
+
 #:
 
