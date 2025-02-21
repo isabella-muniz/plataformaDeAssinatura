e7ce62f1e9e9eae0e2d88cb980fa23555e34986f
isabella-muniz <contato.isabellaramos@hotmail.com>
2025-02-21 12:57:19 +0000

    Arquivos

---
+++ src/account/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        field = (
+            'email',
+            'first_name',
+            'last_name',
+            'password1'
+            'password2'
+            'is_writer'
+        )
